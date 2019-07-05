d215ce6c388fe53a0f4429afe883a2128c28674c
vijaysoni0108 <52547449+vijaysoni0108@users.noreply.github.com>
2019-07-06 01:51:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
  
-int fibonacci(int n)
+double fibonacci_recursive(int n)
 {
     if((n==1)||(n==0))
     {
@@ -9,47 +9,52 @@ int fibonacci(int n)
     }
     else
     {
-        return(fibonacci(n-1)+fibonacci(n-2));
+        return(fibonacci_recursive(n-1)+fibonacci_recursive(n-2));
+    }
+}
+
+void fibonacci_iterative(int n)
+{
+    double first = 0, second = 1, next;
+    for ( int i = 0 ; i < n ; i++ )
+    {
+        if ( i <= 1 )
+        next = i;
+        else
+        {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout <<" "<< next << endl;
     }
 }
  
 int main()
 {
     int choice,n,i=0;
-    cout<<"Input the number of terms for Fibonacci Series:";
+    cout<<"Enter the number of terms for Fibonacci Series:";
     cin>>n;
-	cout<<"Enter choice :  1 - Recursive,       2 - Iterative\n";
-	cin>>choice;
-	if (choice == 1)
-	{
-		cout<<"\nFibonacci Series is as follows\n";
-        while(i<n)
-        {
-           cout<<" "<<fibonacci(i);
-           i++;
-        }
-	}
-	else if (choice == 2)
-	{
-		int first = 0, second = 1, next;
-        cout << "First " << n << " terms of Fibonacci series are :- " << endl;
- 
-        for ( i = 0 ; 1 < n ; i++ )
-        {
-           if ( i <= 1 )
-           next = i;
-           else
-           {
-               next = first + second;
-               first = second;
-               second = next;
-           }
-           cout << next << endl;
-        }
-	}
-	else
-	{
-		cout<<"\nIncorrect choice value - enter only 1 or 2 \n";
-	}
-	return 0;
+    cout<<"Enter choice :  1 - Recursive,       2 - Iterative\n";
+    cin>>choice;
+
+    if (choice == 1)
+    {
+       cout << "First " << n << " Fibonacci series using recursive  function is :- " << endl;
+       while(i<n)
+       {
+          cout<<" "<<fibonacci_recursive(i) << endl;
+          i++;
+       }
+    }
+    else if (choice == 2)
+    {
+        cout << "First " << n << " Fibonacci series using iterative approach is :- " << endl;
+        fibonacci_iterative(n);
+    }
+    else
+    {
+        cout<<"\nIncorrect choice value - enter only 1 or 2 \n";
+    }
+    return 0;
 }
